c26faa2654052bae0e31d8bf8473dfaf08387b3f
Death916 <tavn1992@gmail.com>
2018-09-03 00:59:51 -0700

    get current directory for loggging and os

--- mining_diff_scraper.py
+++ mining_diff_scraper.py
@@ -1,4 +1,3 @@
-#! python3
 # mining_diff_scraper - scrapes whattomine.com for historical mining difficulty
 # nethash and other relevant data for crypto currencies
 
@@ -6,17 +5,20 @@ import json
 import requests
 import pandas as pd
 import time
+import os
+
+FOLDER = os.getcwd()
 
 
 # Print main coin and current difficulty
 def get_coin():
-
 	coin = LBC_data['tag']
 	difficulty = LBC_data['difficulty']
 	price = LBC_data['exchange_rate']
 	print('The Current Difficulty of ' + coin + ' is ' + str(difficulty))
 	print(' LBC/BTC price:' + str(price))
 	timestamp = LBC_data['timestamp']
+
 
 def get_XZC():
 	coin2 = XZC_data['tag']
@@ -37,7 +39,7 @@ def full_info():
 def LBC_to_csv():
 	lbryjson = pd.read_json(LBC.content, typ='series')
 	LBC_df = pd.DataFrame(lbryjson)
-	LBC_df.to_csv('E:\code\python\death916\mining-difficulty\Lbry_History.csv', mode='a')
+	LBC_df.to_csv(FOLDER + '\\' + 'LBC.csv', mode='a')
 	print('saved LBC data to spreadsheet')
 
 
@@ -45,27 +47,24 @@ def xzc_to_csv():
 	XZC = requests.get('http://whattomine.com/coins/175.json')
 	xzcjson = pd.read_json(XZC.content, typ='series')
 	XZC_df = pd.DataFrame(xzcjson)
-	XZC_df.to_csv('E:\code\python\death916\mining-difficulty\XZC_History.csv', mode='a')
+	XZC_df.to_csv(FOLDER + '\\' + 'XZC.csv', mode='a')
 	print('saved XZC data to spreadsheet')
 
 
 def allcoins():
 	allcoins = requests.get('http://whattomine.com/coins.json')
 	alldata = pd.read_json(allcoins.text)
-	alldata.to_csv('E:\code\python\death916\mining-difficulty\Allcoins.csv', mode='a')
+	alldata.to_csv(FOLDER + '\\' + 'allcoins.csv', mode='a')
 	print('allcoins saved to csv')
 
 
 while True:
-
-	
-	
 	try:
 		LBC = requests.get('http://whattomine.com/coins/164.json')
 		XZC = requests.get('http://whattomine.com/coins/175.json')
 		LBC_data = json.loads(LBC.text)
 		XZC_data = json.loads(XZC.text)
-		
+
 		if LBC.status_code == 200:
 			print('Loaded coin data')
 		else:
@@ -73,11 +72,9 @@ while True:
 
 	except BaseException as e:
 		with open("download.log", "a") as logf:
-			logf.write(((str)(e) + time.ctime() + "\n"))
+			logf.write((str(e) + time.ctime() + "\n"))
 		print(e)
-		
 	try:
-		
 		get_coin()
 		get_XZC()
 		LBC_to_csv()
@@ -87,18 +84,9 @@ while True:
 		time.sleep(1800)
 
 	except ValueError as v:
-		print((str)(v))
+		print(str(v))
 		print(v)
 	except BaseException as b:
 		with open("diff.log", "a") as logf:
-			logf.write(((str)(b) + time.ctime() + "\n"))
+			logf.write((str(b) + time.ctime() + "\n"))
 		print(b)
-
-	
-	
-
-
-
-
-
-
